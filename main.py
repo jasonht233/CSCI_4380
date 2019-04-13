e4c3e8d8d156c3209e2920e1c4ce6c82478eadb5
HT <jasonzht233@gmail.com>
2019-04-12 23:10:15 -0400

    basic04/12/23/10
    general structure

--- main.py
+++ main.py
@@ -5,94 +5,105 @@ import load_data
 
 import sys
 
-test = True 
+test = True   
 
-def clean_check( ins_lst ):
-    fine = list()
 
-    for i in ins_lst:
+class engine:
 
-        if i == "Historic" or i == "Outdoor" or i == "Liquor" or i =="Rivers":
-            fine.append(i)
-    
-    return fine 
+    def __init__(self):
+        if not test:
+            load_data.main()
 
-def find_his(loc_x, loc_y):
-    print(">>>doing func find_his()") 
-    return (0,0)
+        #connection from the data 
+        connection_string = "host= 'localhost' dbname='resort' user='resort' password='resort'"
+        self.conn = psycopg2.connect(connection_string,cursor_factory=psycopg2.extras.DictCursor )
 
-def find_out(loc_x, loc_y):
-    print(">>>doing func find_out()")
-    return (0,0)
 
-def find_liq(loc_x, loc_y):
-    print(">>>doing func find_liq()")
-    return (0,0)
+        #main function:
 
-def find_riv(loc_x, loc_y):
-    print(">>>doing func find_riv()")
-    return (0,0)
+        self.loc_x = -1 
+        self.loc_y = -1 
+
+    def clean_check( self, ins_lst ):
+        fine = list()
+
+        for i in ins_lst:
+
+            if i == "Historic" or i == "Outdoor" or i == "Liquor" or i =="Rivers":
+                fine.append(i)
+            else:
+                print("unrecongnized word from the clean check", i)
+        return fine 
+
+    def search(self):
+        while(True):
+
+            if(self.loc_x != -1 or self.loc_y != -1):
+                self.loc_x = input("Please enter your location x: ")
+                self.loc_y = input("Please enter your location y: ")
+
+            # city = input("Please enter your city: ")
+
+            if test:
+                print("the loc_x and loc_y ",self.loc_x,self.loc_y)
+            
+            #to enter the really instruction.
+            instruction = input("Please enter what you want:\n")
+            
+
+
+            if instruction == "Quit":
+                print("thank you for using stupid app")
+                break
+            
+            ins_lst = instruction.split(" ")
+
+            # make sure string is clear , and follow the rules 
+            ins_lst = self.clean_check(ins_lst)
+
+            for ins in ins_lst:
+                
+                if instruction == "Historic":
+                    loc_x,loc_y=self.find_his(loc_x, loc_y)
+
+                if instruction == "Outdoor":
+                    loc_x,loc_y=self.find_out(loc_x, loc_y)
+                
+                if instruction == "Liquor":
+                    loc_x,loc_y=self.find_liq(loc_x, loc_y)
+                
+                if instruction == "Rivers":
+                    loc_x,loc_y=self.find_riv(loc_x, loc_y)
+
+     
+
+    def find_his(self,loc_x, loc_y):
+        print(">>>doing func find_his()") 
+        return (0,0)
+
+    def find_out(self,loc_x, loc_y):
+        print(">>>doing func find_out()")
+        return (0,0)
+
+    def find_liq(self,loc_x, loc_y):
+        print(">>>doing func find_liq()")
+        return (0,0)
+
+    def find_riv(self,loc_x, loc_y):
+        print(">>>doing func find_riv()")
+        return (0,0)
+
+                
+            
 
 if __name__ == "__main__":
+    search_eng = engine()
 
-    if not test:
-        load_data.main()
-
-    #connection from the data 
-    connection_string = "host= 'localhost' dbname='resort' user='resort' password='resort'"
-    conn = psycopg2.connect(connection_string,cursor_factory=psycopg2.extras.DictCursor )
+    search_eng.search() 
 
 
-    #main function:
 
-    loc_x = -1 
-    loc_y = -1 
-    
-    while(True):
-
-        if(loc_x != -1 or loc_y != -1):
-            loc_x = input("Please enter your location x: ")
-            loc_y = input("Please enter your location y: ")
-
-        # city = input("Please enter your city: ")
-
-        if test:
-            print("the loc_x and loc_y ",loc_x,loc_y)
-        
-        #to enter the really instruction.
-        instruction = input("Please enter what you want:\n")
-        
-
-
-        if instruction == "Quit":
-            print("thank you for using stupid app")
-            break
-        
-        ins_lst = instruction.split(" ")
-
-        # make sure string is clear , and follow the rules 
-        ins_lst = clean_check(ins_lst)
-
-        for ins in ins_lst:
-               
-            if instruction == "Historic":
-                loc_x,loc_y=find_his(loc_x, loc_y)
-
-            if instruction == "Outdoor":
-                loc_x,loc_y=find_out(loc_x, loc_y)
             
-            if instruction == "Liquor":
-                loc_x,loc_y=find_liq(loc_x, loc_y)
-            
-            if instruction == "Rivers":
-                loc_x,loc_y=find_riv(loc_x, loc_y)
-            
-        
-
-
-
-
-        
 
 
 
